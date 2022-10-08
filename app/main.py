41ad60932ca97336234b4cb0c99b3fba2db4bb68
shvedbook <37353259+shvedbook@users.noreply.github.com>
2022-10-08 11:48:42 +0300

    Revert "Update main.py"

--- app/main.py
+++ app/main.py
@@ -113,15 +113,6 @@ class Codicological_Data(BaseModel):
 
 app = FastAPI()
 
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 
