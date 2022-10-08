5479e6d967a694502e8bc042394cd9c792f3aa84
Alex Golberg <96111811+alexandergolbergwix@users.noreply.github.com>
2022-10-08 11:29:33 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -113,6 +113,15 @@ class Codicological_Data(BaseModel):
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
