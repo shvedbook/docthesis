4338f7ca3c40d583e856b4a57309e4d1a29d5f33
shvedbook <37353259+shvedbook@users.noreply.github.com>
2022-10-10 18:55:37 +0300

    Revert "Update main.py"

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import uvicorn
-import shap 
+
 import pickle
 
 from fastapi import FastAPI
@@ -7,10 +7,6 @@ from xgboost import XGBRegressor
 from pydantic import BaseModel,Field
 import pandas as pd
 from fastapi.middleware.cors import CORSMiddleware
-
-from io import BytesIO
-from starlette.responses import StreamingResponse
-
 
 feature_names = {'feature_1': 'מיתווה או תבנית (פורמט)',
  'feature_2': 'שמירת סדר הקודקס: שומרי  דפים/גיליונות',
@@ -136,7 +132,6 @@ app.add_middleware(
 
 model = XGBRegressor()
 model.load_model("app/model.txt")
-explainer_xgb = pickle.load(open("app/explainer.pkl", "rb"))
 
 @app.get('/')
 
@@ -157,26 +152,9 @@ def predict_production_year(data: Codicological_Data):
     print(prediction)
     return {'prediction': str(prediction[0])}
    
-@app.post('/getshap')
+@app.post('/post')
 
-def predict_production_year(data: Codicological_Data):
-    received = data.dict()
-    df_dict = {}
-
-    for k in received:
-        df_dict[feature_names[k]] = list(str(received[k]))
-    df = (pd.DataFrame.from_dict(df_dict)).astype(float)
-    prediction = model.predict(df)
-    print(prediction)
+def predict_production_year(data: Text_Post):
+    received = data
     
-    columns_for_showing = []
-    for column in df.columns:
-      new_col = column[::-1]
-      columns_for_showing.append(new_col)
-
-    shap_values = explainer_xgb.shap_values(df)
-    buf = BytesIO()
-    shap.force_plot(explainer_xgb.expected_value, shap_values ,feature_names=columns_for_showing, show=False, matplotlib=True).savefig(buf, format="png")
-    buf.seek(0)
-    
-    return StreamingResponse(buf, media_type="image/png")
+    return {'prediction': received}
